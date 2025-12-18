626cdecc3137bebecfda09d5ff2c5a5c6b1ee9ea
Ivan Nikitin <ivannikitin9853@gmail.com>
2025-12-18 16:40:10 +0700

    add func to base model for naming tables

--- src/todolist_fastapi/core/base.py
+++ src/todolist_fastapi/core/base.py
@@ -1,11 +1,14 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 
+from todolist_fastapi.utils.utils import camel_case_to_snake_case
+
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
-        return f"{cls.__name__.lower()}s"
+        return f"{camel_case_to_snake_case(cls.__name__)}s"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
